cb6d3b67dc2921ddedd81a05bd051ed9124f4263
Saikat Barai <saikatbarai299@gmail.com>
2023-11-01 01:46:18 +0600

    bosus set update

--- app/Models/BonusSets.php
+++ app/Models/BonusSets.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class BonusSets extends Model
 {
     use HasFactory;
+    protected $fillable = ['bonus_type', 'bonus', 'c_wallet', 'r_wallet', 's_wallet'];
 }
